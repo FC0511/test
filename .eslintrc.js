a3600fd1147884e80bc7647ad03132800c431f0e
lonsunFC <2881692705@qq.com>
2020-06-22 15:29:08 +0800

    字体图标使用

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+                                                    'eslint:recommended',
+        '@vue/typescript'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        '@typescript-eslint/no-var-requires': 0
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+};
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+// ts vue svg 报错处理
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
